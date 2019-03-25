ae1882226b97d3d5a2adc891fde25584f4c1efc4
Lutz Moppert <lutz.moppert@kronosww.com>
2019-03-25 15:07:50 +0100

    Added synching for the new websites

--- sync.sh
+++ sync.sh
@@ -1,4 +1,6 @@
 aws s3 sync kronobag s3://kronobag
+aws s3 sync kronomatt s3://kronomatt
 aws s3 sync kronos-ink-grades s3://kronos-ink-grades
 aws s3 sync kronos2066 s3://kronos2066
 aws s3 sync kronos2360 s3://kronos2360
+aws s3 sync kronos4045 s3://kronos4045
